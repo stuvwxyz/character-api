f6c6e091c6735bffe8824fc74b83b3807d73ac36
Redietwol <Redietwol@gmail.com>
2019-02-12 17:46:04 -0600

    first commit

---
+++ src/main/java/org/gordianknots/charactersapi/CharacterApiApplication.java
@@ -0,0 +1,14 @@
+package org.gordianknots.charactersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/gordianknots/charactersapi/CharactersApiApplicationTests.java
@@ -0,0 +1,17 @@
+package org.gordianknots.charactersapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CharactersApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
