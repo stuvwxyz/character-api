dc59dbbf2ce190f89783acbb028727de0964f661
Tony <Tony.Wolyniec@Target.com>
2019-02-28 22:33:26 -0600

    updated path

--- src/main/java/org/gordianknots/charactersapi/controllers/CharacterController.java
+++ src/main/java/org/gordianknots/charactersapi/controllers/CharacterController.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
-@RequestMapping("/character")
+@RequestMapping("/api/character")
 public class CharacterController {
     @Autowired
     private CharacterRepository characterRepository;
