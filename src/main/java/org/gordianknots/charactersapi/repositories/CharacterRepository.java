86bc5acdd9e06f215f7e1d0b5a21bac2547303e1
Tony <Tony.Wolyniec@Target.com>
2019-03-16 12:49:17 -0500

    added sort by name

--- src/main/java/org/gordianknots/charactersapi/repositories/CharacterRepository.java
+++ src/main/java/org/gordianknots/charactersapi/repositories/CharacterRepository.java
@@ -3,6 +3,9 @@ package org.gordianknots.charactersapi.repositories;
 import org.gordianknots.charactersapi.models.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+    public List<Character> findAllByOrderByCharacterNameAsc();
 
 }
